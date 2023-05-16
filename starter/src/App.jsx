54637b509c0ffe034c48e3b62169213e31a4b8ed
1shera1iyev1c <1developer1cccc@gmail.com>
2023-05-16 18:55:48 +0300

    Add code

---
+++ starter/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit,
+      perspiciatis.
+    </div>
+  );
+}
+
+export default App;
